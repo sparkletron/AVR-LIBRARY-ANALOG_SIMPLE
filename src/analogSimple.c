7a7f8c31fe7486b015c8d6f06f9226f815bd9736
Jay Convertino <electrobs@gmail.com>
2024-03-11 18:25:02 -0400

    license change

--- src/analogSimple.c
+++ src/analogSimple.c
@@ -1,32 +1,36 @@
-/*
- * analogSimple.c
+/*******************************************************************************
+ * @file    analogSimple.c
+ * @author  Jay Convertino(electrobs@gmail.com)
+ * @date    2014.11.09
+ * @brief   defines
+ * @details Collection defines
+ * @version 0.0.1
  *
- *  Created on: Nov 9, 2014
- *      Author: John Convertino
+ * @TODO
+ *  - Cleanup interface
  *
- *		Sets up in single conversion mode to sample any analog input selected by getAnalog
+ * @license mit
  *
-    Copyright (C) 2014 John Convertino
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
- *		Version: v0.1
- *		Nov 9, 2014 v0.1	initial setup for single conversion mode
+ * Copyright 2024 Johnathan Convertino
  *
- *		TODO
- */
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
 
 #include <avr/common.h>
 #include <avr/io.h>
@@ -37,38 +41,38 @@
 
 void initAnalog()
 {
-	uint8_t tmpSREG = 0;
+  uint8_t tmpSREG = 0;
 
-	tmpSREG = SREG;
-	cli();
+  tmpSREG = SREG;
+  cli();
 
-	ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-	ADMUX |= (1 << REFS0);
+  ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  ADMUX |= (1 << REFS0);
 
-	SREG = tmpSREG;
+  SREG = tmpSREG;
 }
 
 uint16_t getAnalog(uint8_t channel)
-{
-	uint8_t tmpSREG = 0;
-	//create buffer for analog input value
-	uint16_t buffer = 0;
+  {
+  uint8_t tmpSREG = 0;
+  //create buffer for analog input value
+  uint16_t buffer = 0;
 
-	tmpSREG = SREG;
-	cli();
+  tmpSREG = SREG;
+  cli();
 
-	//Selection of pin to read.
-	ADMUX |= channel;
-	ADMUX &= (MASK_8BIT_FF << 4) | channel;
-	ADCSRA |= (1 << ADSC);
-	//busy wait till conversion complete
-	while(ADCSRA & (1 << ADSC));
-	//read lower eight bits and assign to buffer
-	buffer = ADCL;
-	//than read high bits and shift to correct bit location.
-	buffer |= ADCH << 8;
+  //Selection of pin to read.
+  ADMUX |= channel;
+  ADMUX &= (MASK_8BIT_FF << 4) | channel;
+  ADCSRA |= (1 << ADSC);
+  //busy wait till conversion complete
+  while(ADCSRA & (1 << ADSC));
+  //read lower eight bits and assign to buffer
+  buffer = ADCL;
+  //than read high bits and shift to correct bit location.
+  buffer |= ADCH << 8;
 
-	SREG = tmpSREG;
+  SREG = tmpSREG;
 
-	return buffer;
+  return buffer;
 }
